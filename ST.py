b24f9112d88602464418783427c7a80719ad8e6c
Weston Uribe <westonuribe99@gmail.com>
2018-02-28 10:35:06 -0800

    All data types are able to be downloaded to csv. Need to write SQL to pull csv into database.

--- ST.py
+++ ST.py
@@ -4,18 +4,33 @@ import urllib.request
 import json
 import csv
 import os
-import requests
+import sqlD
+from collections import defaultdict
 
-def writeToCsvFromRows(filename, rows):
-  if len(rows) > 0:
-    with open(filename, 'w') as csvfile:
-      fieldnames = rows[0].keys()
 
-      writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-      writer.writeheader()
+# https://api.iextrading.com/1.0/ref-data/symbols
+# https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,tsla&types=chart
 
-      for row in rows:
-        writer.writerow(row)
+# https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,tsla&types=chart&range=5y
+
+
+def writeToCsvFromRows(filename, rows, dataType):
+  if dataType == "chart":
+    fieldnames = ["date","open","high","low","close","volume","unadjustedVolume","change","changePercent","vwap","label","changeOverTime","symbol"]
+  elif dataType == "quote":
+    fieldnames = ["symbol","companyName","primaryExchange","sector","calculationPrice","open","openTime","close","closeTime","high","low","latestPrice","latestSource","latestTime","latestUpdate","latestVolume","iexRealtimePrice","iexRealtimeSize","iexLastUpdated","delayedPrice","delayedPriceTime","previousClose","change","changePercent","iexMarketPercent","iexVolume","avgTotalVolume","iexBidPrice","iexBidSize","iexAskPrice","iexAskSize","marketCap","peRatio","week52High","week52Low","ytdChange"]
+  elif dataType == "stats":
+    fieldnames = ["companyName","marketcap","beta","week52high","week52low","week52change","shortInterest","shortDate","dividendRate","dividendYield","exDividendDate","latestEPS","latestEPSDate","sharesOutstanding","float","returnOnEquity","consensusEPS","numberOfEstimates","symbol","EBITDA","revenue","grossProfit","cash","debt","ttmEPS","revenuePerShare","revenuePerEmployee","peRatioHigh","peRatioLow","EPSSurpriseDollar","EPSSurprisePercent","returnOnAssets","returnOnCapital","profitMargin","priceToSales","priceToBook","day200MovingAvg","day50MovingAvg","institutionPercent","insiderPercent","shortRatio","year5ChangePercent","year2ChangePercent","year1ChangePercent","ytdChangePercent","month6ChangePercent","month3ChangePercent","month1ChangePercent","day30ChangePercent","day5ChangePercent"]
+  else:
+    print("Error: Wrong data type.")
+  
+  with open(filename, 'w') as csvfile:
+
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for row in rows:
+      writer.writerow(row)
 
 # Takes in a jsonArray and returns a list of strings
 def jsonArrayToSymbolNames(jsonArray):
@@ -34,7 +49,7 @@ def gatherSymbols():
   webUrl = urllib.request.urlopen(urlData)
   # Verify that it works
   if (webUrl.getcode() == 200):
-    print ("Connected and gathering symbols...")
+    print ("Connected to URL and gathering symbols...")
     symbolArray = webUrl.read()
     
     rows = json.loads(symbolArray)
@@ -46,7 +61,7 @@ def gatherSymbols():
     print ("Received an error from server, cannot retrieve results " + str(webUrl.getcode()))
 
 
-def cleanData(jsonObject, dataType):
+def cleanDictData(jsonObject, dataType):
   rows = []
   for key in jsonObject:
     data = jsonObject[key][dataType]
@@ -62,49 +77,78 @@ def getData(symbols,dataType):
   if (webUrl.getcode() == 200):
     data = webUrl.read()
     # print cleaned data
-    rows = cleanData(json.loads(data), dataType)
+    rows = cleanDictData(json.loads(data), dataType)
     # write results to csv
-    directory = "poophead/" + symbols[0]
+    directory = "tempData/" + symbols[0]
     if not os.path.exists(directory):
       os.makedirs(directory)
     filename = directory + "/" + dataType.replace('/','_') + ".csv"
     print("Downloading data to " + filename)
-    writeToCsvFromRows(filename, rows)
-    # insert rows into sql
+    writeToCsvFromRows(filename, rows, dataType)
+  
   else:
-    print ("Received an error from server, cannot retrieve results " + str(webUrl.getcode()))
-    
+    print ("Received an error from server, cannot retrieve quote or stats data " + str(webUrl.getcode()))
+
+
+   
+def cleanChartArray(jsonObject):
+  rows = []
+  for symbol in jsonObject:
+    data = jsonObject[symbol]["chart"]
+    for day in data:
+      day["symbol"] = symbol
+    rows += data
+  # Change importLastChartRow to take in symbol so that it returns the last row per symbol
+  # Put the following in the for loop above per symbol 
+  latestChartRow = sqlD.importLastChartRow()
+  
+  latestChartDate = "1970-01-23"
+  if latestChartRow:
+    latestChartDate = latestChartRow[0][0]
+
+  chartDataForDaysMissed = [row for row in rows if row["date"] > latestChartDate]
+ 
+  return chartDataForDaysMissed
+ 
+ 
+  
 def updateChartData(symbols):
   # Grab the URL
-  urlData = "https://api.iextrading.com/1.0/stock/market/batch?symbols=" + ",".join(symbols) + "&types=chart/5y"
+  urlData = "https://api.iextrading.com/1.0/stock/market/batch?symbols=" + ",".join(symbols) + "&types=chart&range=5y"
   # Open the URL and read the data
   webUrl = urllib.request.urlopen(urlData)
   # Verify that it works
   if (webUrl.getcode() == 200):
     print ("Updating chart table...")
-    symbolArray = webUrl.read()
+    chartArray = webUrl.read()
+    rows = json.loads(chartArray)
     
-    rows = json.loads(symbolArray)
-    
-    chartDataForDaysMissed = cleanChartData(rows)
+    chartDataForDaysMissed = cleanChartArray(rows)
+    if chartDataForDaysMissed:
+      ### Print results to .csv
+      directory = "tempData/" + symbols[0]
+      if not os.path.exists(directory):
+        os.makedirs(directory)
+      filename = directory + "/chart.csv"
+      print("Downloading data to " + filename)
+      writeToCsvFromRows(filename, chartDataForDaysMissed, "chart")
+    else:
+      print("Chart data is up to date.")
     
   else:
-    print ("Received an error from server, cannot retrieve results " + str(webUrl.getcode()))
-
-def cleanChartData(rows):
-  # Use Sql-quoteTable "latestTime": "September 19, 2017", to find date since last execution
-  dateSinceLastExecution
-  
-  return chartDataForDaysMissed 
-  # Keep only data for days needed for update
-  # Print results to .csv
+    print ("Received an error from server, cannot retrieve chart data " + str(webUrl.getcode()))
 
 
 if __name__ == "__main__":
+  
+  # Create a database if none exists
+  sqlD.createSqlTables()
+  
   stepSize=100
   symbolNames = gatherSymbols()
 
   for i in range(0, len(symbolNames), stepSize):
-    #getData(symbolNames[i:i+stepSize],"quote")
-    #getData(symbolNames[i:i+stepSize],"stats")
     updateChartData(symbolNames[i:i+stepSize])
+    getData(symbolNames[i:i+stepSize],"quote")
+    getData(symbolNames[i:i+stepSize],"stats")
+    
--- sqlD.py
+++ sqlD.py
@@ -23,18 +23,48 @@ def create_table(conn, create_table_sql):
     print(e)
 
 # Creates tables
-def createSqlTable():
-  database = "pythonsqlite.db"
-  
-  sql_create_quote_table = """CREATE TABLE IF NOT EXISTS quote (symbol TEXT PRIMARY KEY, companyName text, primaryExchange text, sector text, calculationPrice text, open real, openTime integer, close real, closeTime integer, high real, low real, latestPrice real, latestSource real, latestTime text, latestUpdate integer, latestVolume integer, iexRealtimePrice real, iexRealtimeSize real, iexLastUpdated text, delayedPrice real, delayedPriceTime integer, previousClose real, change real, changePercent real, iexMarketPercent real, iexVolume integer, avgTotalVolume integer, iexBidPrice real, iexBidSize integer, iexAskPrice real, iexAskSize real, marketCap integer, peRatio real, week52High real, week52Low real, ytdChange real);"""
-  
+def createSqlTables():
+  database = "stockInfo.db"
+  sql_create_chart_table = """CREATE TABLE IF NOT EXISTS chart (symbol text PRIMARY KEY, date text, open real, high real, low real, close real, volume integer, unadjustedVolume integer, change real, changePercent real, vwap real, label text, changeOverTime real);"""
+  sql_create_quote_table = """CREATE TABLE IF NOT EXISTS quote (symbol text PRIMARY KEY, companyName text, primaryExchange text, sector text, calculationPrice text, open real, openTime integer, close real, closeTime integer, high real, low real, latestPrice real, latestSource real, latestTime text, latestUpdate integer, latestVolume integer, iexRealtimePrice real, iexRealtimeSize real, iexLastUpdated text, delayedPrice real, delayedPriceTime integer, previousClose real, change real, changePercent real, iexMarketPercent real, iexVolume integer, avgTotalVolume integer, iexBidPrice real, iexBidSize integer, iexAskPrice real, iexAskSize real, marketCap integer, peRatio real, week52High real, week52Low real, ytdChange real, FOREIGN KEY(symbol) REFERENCES chart(symbol));"""
+  sql_create_stats_table = """CREATE TABLE IF NOT EXISTS stats (companyName text, marketcap integer, beta real, week52high real, week52low real, week52change real, shortInterest integer, shortDate text, dividendRate real, dividendYield real, exDividendDate text, latestEPS real, latestEPSDate text, sharesOutstanding integer, float integer, returnOnEquity real, consensusEPS real, numberOfEstimates integer, symbol text, EBITDA integer, revenue integer, grossProfit integer, cash integer, debt integer, ttmEPS real, revenuePerShare real, revenuePerEmployee real, peRatioHigh real, peRatioLow real, EPSSurpriseDollar real, EPSSurprisePercent real, returnOnAssets real, returnOnCapital real, profitMargin real, priceToSales real, priceToBook real, day200MovingAvg real, day50MovingAvg real, institutionPercent real, insiderPercent real, shortRatio real, year5ChangePercent real, year2ChangePercent real, year1ChangePercent real, ytdChangePercent real, month6ChangePercent real, month3ChangePercent real, month1ChangePercent real, day5ChangePercent real, FOREIGN KEY(symbol) REFERENCES chart(symbol));"""
   # create a database connection
   conn = create_connection(database)
   if conn is not None:
-    # create projects table
+    # create tables
+    create_table(conn, sql_create_chart_table)
     create_table(conn, sql_create_quote_table)
+    create_table(conn, sql_create_stats_table)
   else:
     print("Error! Cannot create the database connection.")
 
+
+
+
+
+def select_lastrow_chart(conn):
+    """
+    Query last row in the chart table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM chart ORDER BY date DESC LIMIT 1")
+ 
+    rows = cur.fetchall()
+    
+    return(rows)
+
+def importLastChartRow():
+  database = "stockInfo.db"
+ 
+    # create a database connection
+  conn = create_connection(database)
+  with conn:
+    print("Querying last row in chart...")
+    return select_lastrow_chart(conn)
+
+
 if __name__ == "__main__":
-  createSqlTable()
+  createSqlTables()
+  print(importLastChartRow())
